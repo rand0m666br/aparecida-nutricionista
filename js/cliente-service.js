adaa9eb70db82629e56f2c0221b764e2caee15fb
rand0m <heitor.vieira.juliano@gmail.com>
2024-03-20 11:45:22 -0300

    first commit

---
+++ js/cliente-service.js
@@ -0,0 +1,30 @@
+const novaLinha = (nome, peso, altura, gordura, imc) => {
+    const linhaNovoPaciente = document.createElement("tr");
+    linhaNovoPaciente.classList.add("paciente");
+
+    const conteudo = `
+    <td class="info-nome">${nome}</td>
+    <td class="info-peso">${peso}</td>
+    <td class="info-altura">${altura}</td>
+    <td class="info-gordura">${gordura}</td>
+    <td class="info-imc">${imc}</td>
+    `;
+
+    linhaNovoPaciente.innerHTML = conteudo;
+    return linhaNovoPaciente;
+}
+const tabela = document.querySelector("#tabela-pacientes");
+
+const http = new XMLHttpRequest();
+ 
+http.open('GET', 'http://localhost:3000/profile');
+ 
+http.send();
+
+http.onload = () => {
+    const data = JSON.parse(http.response);
+
+    data.forEach(element => {
+        tabela.appendChild(novaLinha(element.nome, element.peso, element.altura, element.gordura, element.imc));
+    });
+}
